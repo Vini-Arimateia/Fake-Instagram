353bd4dda6ed5cd676af114e93953081eacd3213
Vini-Arimateia <arimateiavini13@gmail.com>
2024-12-03 13:36:43 -0300

    Add files via upload

---
+++ projeto instagram/stories.js
@@ -0,0 +1,60 @@
+var crl1 = document.getElementById('myCrl1').getContext('2d');
+
+crl1.beginPath();
+crl1.arc(60,100,40,0,2 *Math.PI); //criar círculo;
+crl1.fillStyle = "#000000"; //cor do circulo
+crl1.fill();
+crl1.linewidth = 5;
+crl1.StrokeStyle = "#000000"; 
+crl1.stroke();
+
+var crl2 = document.getElementById('myCrl2').getContext ('2d');
+
+crl2.beginPath();
+crl2.arc(60,100,40,0,2 *Math.PI); //criar círculo;
+crl2.fillStyle = "#000000"; //cor do circulo
+crl2.fill();
+crl2.linewidth = 5;
+crl2.StrokeStyle = "#000000"; 
+crl2.stroke();
+
+var crl3 = document.getElementById('myCrl3').getContext ('2d');
+
+crl3.beginPath();
+crl3.arc(60,100,40,0,2 *Math.PI); //criar círculo;
+crl3.fillStyle = "#000000"; //cor do circulo
+crl3.fill();
+crl3.linewidth = 5;
+crl3.StrokeStyle = "#000000"; 
+crl3.stroke();
+
+var crl4 = document.getElementById('myCrl4').getContext ('2d');
+
+crl4.beginPath();
+crl4.arc(60,100,40,0,2 *Math.PI); //criar círculo;
+crl4.fillStyle = "#000000"; //cor do circulo
+crl4.fill();
+crl4.linewidth = 5;
+crl4.StrokeStyle = "#000000"; 
+crl4.stroke();
+
+var crl5 = document.getElementById('myCrl5').getContext ('2d');
+
+crl5.beginPath();
+crl5.arc(60,100,40,0,2 *Math.PI); //criar círculo;
+crl5.fillStyle = "#000000"; //cor do circulo
+crl5.fill();
+crl5.linewidth = 5;
+crl5.StrokeStyle = "#000000"; 
+crl5.stroke();
+
+var crl6 = document.getElementById('myCrl6').getContext ('2d');
+
+crl6.beginPath();
+crl6.arc(60,100,40,0,2 *Math.PI); //criar círculo;
+crl6.fillStyle = "#000000"; //cor do circulo
+crl6.fill();
+crl6.linewidth = 5;
+crl6.StrokeStyle = "#000000"; 
+crl6.stroke();
+
